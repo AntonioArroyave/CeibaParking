1efb783dc53cad67e117e3e872548454160c0bd4
Rafael Antonio Barrientos Arroyave <rafaelbarrientosarroyave@gmail.com>
2018-06-13 10:10:37 -0500

    cambio a spring

---
+++ src/main/java/com/ceiba/CeibaParking/CeibaParkingApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.CeibaParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaParkingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ceiba/CeibaParking/CeibaParkingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ceiba.CeibaParking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CeibaParkingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
