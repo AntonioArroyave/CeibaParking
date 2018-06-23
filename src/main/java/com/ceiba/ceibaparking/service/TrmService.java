8158846a3d7ee2ee7dc91cc8f39b89d2c553d00e
Antonio Arroyave <rafaelbarrientosarroyave@gmail.com>
2018-06-23 10:08:22 -0500

    TRM

---
+++ src/main/java/com/ceiba/ceibaparking/service/TrmService.java
@@ -0,0 +1,11 @@
+package com.ceiba.ceibaparking.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+import TRM.wsdl.GetQuote;
+import hello.wsdl.GetQuoteResponse;
+public class TrmService {
+
+}
